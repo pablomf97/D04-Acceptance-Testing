4904afeff13c51f549180abd3019150f4064bee6
chema <joslopcar@alum.us.es>
2019-05-11 17:46:47 +0200

    arreglo del test de personaldata

--- workspace/Acme-Hacker-Rank/src/test/java/services/PersonalDataServiceTest.java
+++ workspace/Acme-Hacker-Rank/src/test/java/services/PersonalDataServiceTest.java
@@ -1,3 +1,4 @@
+
 package services;
 
 import java.util.Collection;
@@ -11,39 +12,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import domain.Curricula;
-import domain.Rookie;
-import domain.MiscellaneousData;
-import domain.PersonalData;
-
 import utilities.AbstractTest;
+import domain.Curricula;
+import domain.PersonalData;
+import domain.Rookie;
 
-@ContextConfiguration(locations = { "classpath:spring/junit.xml" })
+@ContextConfiguration(locations = {
+	"classpath:spring/junit.xml"
+})
 @RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
-public class PersonalDataServiceTest  extends AbstractTest{
+public class PersonalDataServiceTest extends AbstractTest {
 
-	
 	@Autowired
-	private ActorService actorService;
-	
-	@Autowired 
-	private CurriculaService curriculaService;
-	
+	private ActorService		actorService;
+
 	@Autowired
-	private PersonalDataService personalDataService;
-	
+	private CurriculaService	curriculaService;
+
+	@Autowired
+	private PersonalDataService	personalDataService;
+
+
 	//Caso de uso 17.1
-	
 
 	/*
 	 * Total coverage of all tests
 	 * 
 	 * 
-	 * Coverage of the total project (%): 
+	 * Coverage of the total project (%):
 	 * 
 	 * 
-	 * Coverage of the total project (lines of codes): 
+	 * Coverage of the total project (lines of codes):
 	 * 
 	 * ################################################################
 	 * 
@@ -70,51 +70,50 @@ public class PersonalDataServiceTest  extends AbstractTest{
 
 	@Test
 	public void driverCreatePersonalData() {
-		Object testingData[][] = {
+		final Object testingData[][] = {
 
-				
+			/* Test 1.1 ----------------------------------------------- */
+			{
+				"rookie1", "https://github.com/fsfd", "https://www.linkedin.com/in/fswe12fd", "Carlos Plaza", "statement1", "618922568", null
+			//Positive
 
-				/* Test 1.1 ----------------------------------------------- */
-				{ "rookie1", "https://github.com/fsfd", "https://linkedln.com/fswe12fd","Carlos Plaza","statement1","618922568", null //Positive
+			},
 
-				},
+			/* Test 1.2 ----------------------------------------------- */
+			{
+				"rookie1", null, "https://www.linkedin.com/in/fswe12fd", "Carlos Plaza", "statement1", "618922568", IllegalArgumentException.class
+			//Negative: attribute githubProfile is null
 
-				/* Test 1.2 ----------------------------------------------- */
-				{ "rookie1", null, "https://linkedln.com/fswe12fd","Carlos Plaza","statement1","618922568", IllegalArgumentException.class //Negative: attribute githubProfile is null
+			},
 
-				},
+			/* Test 1.3 ----------------------------------------------- */
+			{
+				"rookie1", "url", "url", "Carlos Plaza", "statement1", "618922568", ConstraintViolationException.class
+			//Negative: format error url
 
-				/* Test 1.3 ----------------------------------------------- */
-				{ "rookie1","url", "url","Carlos Plaza","statement1","618922568", ConstraintViolationException.class//Negative: format error url
+			},
 
-				},
+			/* Test 2.1 ----------------------------------------------- */
+			{
+				null, "https://github.com/fsfd", "https://www.linkedin.com/in/fswe12fd", "Carlos Plaza", "statement1", "618922568", IllegalArgumentException.class
+			//Negative: actor null
 
+			},
 
+			/* Test 2.2 ----------------------------------------------- */
+			{
+				"company1", "https://github.com/fsfd", "https://www.linkedin.com/in/fswe12fd", "Carlos Plaza", "statement1", "618922568", ClassCastException.class
+			//Negative: actor non authorized
 
-				/* Test 2.1 ----------------------------------------------- */
-				{ null,"https://github.com/fsfd", "https://linkedln.com/fswe12fd","Carlos Plaza","statement1","618922568", IllegalArgumentException.class //Negative: actor null
-
-				},
-
-				/* Test 2.2 ----------------------------------------------- */
-				{ "company1","https://github.com/fsfd", "https://linkedln.com/fswe12fd","Carlos Plaza","statement1","618922568", ClassCastException.class //Negative: actor non authorized
-
-				},
-
-
+			},
 
 		};
 
 		for (int i = 0; i < testingData.length; i++)
-			this.templateCreatePersonalData((String) testingData[i][0],
-					(String) testingData[i][1], (String) testingData[i][2],
-					(String) testingData[i][3], (String) testingData[i][4],
-					 (String) testingData[i][5],
-					(Class<?>) testingData[i][6]);
+			this.templateCreatePersonalData((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (String) testingData[i][3], (String) testingData[i][4], (String) testingData[i][5], (Class<?>) testingData[i][6]);
 	}
 
-	protected void templateCreatePersonalData(String username,
-			String githubProfile, String linkedIn, String fullName, String statement, String phoneNumber, Class<?> expected) {
+	protected void templateCreatePersonalData(final String username, final String githubProfile, final String linkedIn, final String fullName, final String statement, final String phoneNumber, final Class<?> expected) {
 		Rookie principal;
 		Class<?> caught = null;
 
@@ -123,10 +122,9 @@ public class PersonalDataServiceTest  extends AbstractTest{
 
 			principal = (Rookie) this.actorService.findByPrincipal();
 
-			Collection<Curricula>principalCurriculas = this.curriculaService.getCurriculasByRookie(principal.getId());
+			final Collection<Curricula> principalCurriculas = this.curriculaService.getCurriculasByRookie(principal.getId());
 
-			PersonalData pd= this.personalDataService
-					.create();
+			final PersonalData pd = this.personalDataService.create();
 
 			pd.setGithubProfile(githubProfile);
 			pd.setLinkedIn(linkedIn);
@@ -134,58 +132,50 @@ public class PersonalDataServiceTest  extends AbstractTest{
 			pd.setStatement(statement);
 			pd.setPhoneNumber(phoneNumber);
 
-			this.personalDataService.save(pd,principalCurriculas.iterator().next().getId());
+			this.personalDataService.save(pd, principalCurriculas.iterator().next().getId());
 
 			this.personalDataService.flush();
 
 			this.unauthenticate();
 
-		} catch (Throwable oops) {
+		} catch (final Throwable oops) {
 			caught = oops.getClass();
 		}
 		super.checkExceptions(expected, caught);
 	}
 
-
-
-	
-	
 	/*
-	 * ####################### TEST EDIT  PersonalData#######################
+	 * ####################### TEST EDIT PersonalData#######################
 	 */
 
 	@Test
 	public void driverEditPersonalData() {
-		Object testingData[][] = {
+		final Object testingData[][] = {
 
+			/* Test 1.1 ----------------------------------------------- */
+			{
+				"rookie2", "https://github.com/fsfd", "https://www.linkedin.com/in/fswe12fd", "Jesús Plaza", "statement1", "618922568", null
+			//Positive
+			},
 
+			/* Test 1.3 ----------------------------------------------- */
+			{
+				"rookie2", "url", "https://www.linkedin.com/in/fswe12fd", "Jesús Plaza", "statement1", "618922568", ConstraintViolationException.class
+			//Negative: formatt error url
 
-				/* Test 1.1 ----------------------------------------------- */
-				{ "rookie2", "https://github.com/fsfd", "https://linkedln.com/fswe12fd","Jesús Plaza","statement1","618922568", null //Positive
-				},
+			}, {
+				"company1", "https://github.com/fsfd", "https://www.linkedin.com/in/fswe12fd", "Jesús Plaza", "statement1", "618922568", ClassCastException.class
+			// Negative: actor non authorized
 
-				/* Test 1.3 ----------------------------------------------- */
-				{ "rookie2", "url", "https://linkedln.com/fswe12fd","Jesús Plaza","statement1","618922568",
-					ConstraintViolationException.class //Negative: formatt error url
-
-				},
-				{ "company1", "https://github.com/fsfd", "https://linkedln.com/fswe12fd","Jesús Plaza","statement1","618922568",
-					ClassCastException.class // Negative: actor non authorized
-
-				}
+			}
 
 		};
 
 		for (int i = 0; i < testingData.length; i++)
-			this.templateEditPersonalData((String) testingData[i][0],
-					(String) testingData[i][1], (String) testingData[i][2],
-					(String) testingData[i][3], (String) testingData[i][4],
-					 (String) testingData[i][5],
-					(Class<?>) testingData[i][6]);
+			this.templateEditPersonalData((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (String) testingData[i][3], (String) testingData[i][4], (String) testingData[i][5], (Class<?>) testingData[i][6]);
 	}
 
-	protected void templateEditPersonalData(String username,
-			String githubProfile, String linkedIn, String fullName, String statement, String phoneNumber, Class<?> expected) {
+	protected void templateEditPersonalData(final String username, final String githubProfile, final String linkedIn, final String fullName, final String statement, final String phoneNumber, final Class<?> expected) {
 		Rookie principal;
 		Class<?> caught = null;
 
@@ -193,26 +183,25 @@ public class PersonalDataServiceTest  extends AbstractTest{
 			this.authenticate(username);
 			principal = (Rookie) this.actorService.findByPrincipal();
 
-			Collection<Curricula>principalCurriculas = this.curriculaService.getCurriculasByRookie(principal.getId());
-			PersonalData pd = principalCurriculas.iterator().next().getPersonalData();
-			
+			final Collection<Curricula> principalCurriculas = this.curriculaService.getCurriculasByRookie(principal.getId());
+			final PersonalData pd = principalCurriculas.iterator().next().getPersonalData();
+
 			pd.setGithubProfile(githubProfile);
 			pd.setLinkedIn(linkedIn);
 			pd.setFullName(fullName);
 			pd.setStatement(statement);
 			pd.setPhoneNumber(phoneNumber);
 
-			this.personalDataService.save(pd,principalCurriculas.iterator().next().getId());
+			this.personalDataService.save(pd, principalCurriculas.iterator().next().getId());
 
 			this.personalDataService.flush();
 
 			this.unauthenticate();
 
-		} catch (Throwable oops) {
+		} catch (final Throwable oops) {
 			caught = oops.getClass();
 		}
 		super.checkExceptions(expected, caught);
 	}
-	
 
 }
